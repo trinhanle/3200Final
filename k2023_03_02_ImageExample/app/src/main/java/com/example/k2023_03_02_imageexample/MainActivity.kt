4e567de8ed01773e2c061e975d623467c5c02211
Bradford <phillip.g.bradford@gmail.com>
2023-03-04 10:57:01 -0500

    image example

---
+++ k2023_03_02_ImageExample/app/src/main/java/com/example/k2023_03_02_imageexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.k2023_03_02_imageexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
