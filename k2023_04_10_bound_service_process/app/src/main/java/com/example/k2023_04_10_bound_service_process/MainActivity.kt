d2096c5e46f657b059423d631d3a8b436dcf1afd
Bradford <phillip.g.bradford@gmail.com>
2024-02-01 14:20:40 -0500

    Init

---
+++ k2023_04_10_bound_service_process/app/src/main/java/com/example/k2023_04_10_bound_service_process/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.k2023_04_10_bound_service_process
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ k2024_01_30a/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "k2024_01_30a"
+include(":app")
