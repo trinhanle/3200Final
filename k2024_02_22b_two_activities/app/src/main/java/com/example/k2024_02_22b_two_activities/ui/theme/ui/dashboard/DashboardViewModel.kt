3624a771c6a7d042d783b653650d84468ba2adb3
Bradford <phillip.g.bradford@gmail.com>
2024-02-27 11:33:27 -0500

    Init

---
+++ k2024_02_22b_two_activities/app/src/main/java/com/example/k2024_02_22b_two_activities/ui/theme/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.k2024_02_22b_two_activities.ui.theme.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
