8167432c63af0e7f44eb1985c7ed3bad7f5207ab
Bradford <phillip.g.bradford@gmail.com>
2024-03-29 14:13:59 -0400

    Init

--- k2024_02_28_affirmation_app/app/src/main/java/com/example/k2024_02_28_affirmation_app/model/Affirmation.kt
+++ k2024_02_28_affirmation_app/app/src/main/java/com/example/k2024_02_28_affirmation_app/model/Affirmation.kt
@@ -4,6 +4,7 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
 data class Affirmation (
+    val id: Int,
     @StringRes val stringResourceID: Int,
     @DrawableRes val imageResourceID: Int
 )
--- k2024_03_19_basic_radio2/app/src/main/java/com/example/k2024_03_19_basic_radio2/model/RadioStations.kt
+++ k2024_03_19_basic_radio2/app/src/main/java/com/example/k2024_03_19_basic_radio2/model/RadioStations.kt
@@ -9,7 +9,11 @@ class RadioStations {
 
     private val urls =  listOf(
         "http://playerservices.streamtheworld.com/api/livestream-redirect/WNPRFM.mp3",
-        "http://stream.whus.org:8000/whusfm")
+        "http://stream.whus.org:8000/whusfm",
+        "http://amber.streamguys.com:6050/live",
+        "http://playerservices.streamtheworld.com/api/livestream-redirect/WPLRFM.mp3",
+        "http://playerservices.streamtheworld.com/api/livestream-redirect/WEZNFM.mp3"
+        )
 
     private var currentUrl = 0
 
