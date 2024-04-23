935e4a2e33f4bcc2c1bb358e774798dd74891c50
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2024-04-22 22:29:03 -0400

    Delete k2025_02_01 directory

--- k2025_02_01/app/src/main/java/com/example/k2025_02_01/model/Difficulty.kt
+++
@@ -1,5 +0,0 @@
-package com.example.k2025_02_01.model
-
-enum class Difficulty {
-    EASY, MEDIUM, HARD
-}
--- k2025_02_01/app/src/main/java/com/example/k2025_02_01/model/Question.kt
+++
@@ -1,7 +0,0 @@
-package com.example.k2025_02_01.model
-
-data class Question<T>(
-    val questionText: String,
-    val answer: T,
-    val difficulty: Difficulty
-)
