4fdeda967cff950f5610125172784d0f6e768039
trinhanle <59145439+trinhanle@users.noreply.github.com>
2024-05-01 10:53:36 -0400

    Delete k2024_04_09_video_svc directory

--- k2024_04_09_video_svc/app/src/main/java/com/example/k2024_04_09_video_svc/models/Bindings.kt
+++
@@ -1,8 +0,0 @@
-package com.example.k2024_04_09_video_svc.models
-
-import com.example.k2024_04_09_video_svc.databinding.ActivityMainBinding
-
-data class Bindings(val binding: ActivityMainBinding) {
-
-
-}
