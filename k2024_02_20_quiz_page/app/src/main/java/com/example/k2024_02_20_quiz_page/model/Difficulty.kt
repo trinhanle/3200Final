b8f0ea278c34f773519562d3b6712d0013cec8bc
trinhanle <59145439+trinhanle@users.noreply.github.com>
2024-05-01 10:47:43 -0400

    Delete k2024_02_20_quiz_page directory

--- k2024_02_20_quiz_page/app/src/main/java/com/example/k2024_02_20_quiz_page/model/Difficulty.kt
+++
@@ -1,5 +0,0 @@
-package com.example.k2024_02_20_quiz_page.model
-
-enum class Difficulty {
-    EASY, MEDIUM, HARD
-}
--- k2024_02_20_quiz_page/app/src/main/java/com/example/k2024_02_20_quiz_page/model/Question.kt
+++
@@ -1,7 +0,0 @@
-package com.example.k2024_02_20_quiz_page.model
-
-data class Question<T> (
-    val questionText: String,
-    val answer: T,
-    val difficulty: Difficulty
-)
