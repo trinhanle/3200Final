da80588594672ea8e9b13267d259d23a832f8af6
trinhanle <59145439+trinhanle@users.noreply.github.com>
2024-05-01 10:50:47 -0400

    Delete k2024_02_06_quiz_start directory

--- k2024_02_06_quiz_start/app/src/main/java/com/example/k2024_02_06_quiz_start/model/Difficulty.kt
+++
@@ -1,5 +0,0 @@
-package com.example.k2024_02_06_quiz_start.model
-
-enum class Difficulty {
-    EASY, MEDIUM, HARD
-}
--- k2024_02_06_quiz_start/app/src/main/java/com/example/k2024_02_06_quiz_start/model/Question.kt
+++
@@ -1,7 +0,0 @@
-package com.example.k2024_02_06_quiz_start.model
-
-data class Question<T> (
-    val questionText: String,
-    val answer: T,
-    val difficulty: Difficulty
-)
