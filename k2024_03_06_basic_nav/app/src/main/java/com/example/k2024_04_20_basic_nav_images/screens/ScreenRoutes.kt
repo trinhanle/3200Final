720f39380b1a1b75b8f8e087d5cc7b8751e336aa
trinhanle <59145439+trinhanle@users.noreply.github.com>
2024-05-01 10:48:31 -0400

    Delete k2024_03_06_basic_nav directory

--- k2024_03_06_basic_nav/app/src/main/java/com/example/k2024_04_20_basic_nav_images/screens/ScreenRoutes.kt
+++
@@ -1,8 +0,0 @@
-package com.example.k2024_04_20_basic_nav_images.screens
-
-open class ScreenRoutes(val route: String) {
-    object MainScreen: ScreenRoutes("main_screen")
-    object LeftScreen: ScreenRoutes("left_screen")
-    object RightScreen: ScreenRoutes("right_screen")
-}
-
