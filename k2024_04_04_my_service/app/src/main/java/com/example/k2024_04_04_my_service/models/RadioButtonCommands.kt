e24bb2e3aa14ff518a9127aebc00f57ec63ae44e
trinhanle <59145439+trinhanle@users.noreply.github.com>
2024-05-01 10:48:46 -0400

    Delete k2024_04_04_my_service directory

--- k2024_04_04_my_service/app/src/main/java/com/example/k2024_04_04_my_service/models/RadioButtonCommands.kt
+++
@@ -1,5 +0,0 @@
-package com.example.k2024_04_04_my_service.models
-
-enum class RadioButtonCommands {
-    RADIO_ON, RADIO_PAUSE, RADIO_OFF, RADIO_NEXT_STATION
-}
--- k2024_04_04_my_service/app/src/main/java/com/example/k2024_04_04_my_service/models/RadioState.kt
+++
@@ -1,4 +0,0 @@
-package com.example.k2024_04_04_my_service.models
-
-data class RadioState(var prepared: Boolean,
-                      var running: Boolean)
